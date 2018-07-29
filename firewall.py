2035a40df28c52b9134946b8f3c60809d81507de
Bennett Yardley <33298178+bennettyardley@users.noreply.github.com>
2018-07-29 13:10:38 -0400

    Update firewall.py

--- firewall.py
+++ firewall.py
@@ -6,7 +6,7 @@ import pydivert
 w = pydivert.WinDivert("tcp.DstPort == 443 and tcp.PayloadLength > 0")
 w.open()
 
-while True:
+while :
     packet = w.recv()
     print(packet)
     #w.send(packet)
