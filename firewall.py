f44606f8cd4ba3b177ee7655149e9bbf378763d7
Bennett Yardley <33298178+bennettyardley@users.noreply.github.com>
2018-07-30 12:55:25 -0400

    Update firewall.py

--- firewall.py
+++ firewall.py
@@ -3,7 +3,7 @@ import pydivert
 #choose ports
 #choose incoming or outgoing traffic
 
-w = pydivert.WinDivert("tcp.DstPort == 443 and tcp.PayloadLength > 0")
+w = .WinDivert("tcp.DstPort == 443 and tcp.PayloadLength > 0")
 w.open()
 
 while True:
